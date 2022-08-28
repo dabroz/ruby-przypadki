8486ef2eee135ae464431a7b142f1389e4e929ff
Tomasz Pędraszewski <t.pedraszewski@28byteslater.com>
2022-08-28 21:25:05 +0200

    init

---
+++ lib/ruby/przypadki.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "przypadki/version"
+
+module Ruby
+  module Przypadki
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end

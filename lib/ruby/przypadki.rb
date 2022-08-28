bfc3668899ee19b95737e29e4a56b72b8f314581
Tomasz Pędraszewski <t.pedraszewski@28byteslater.com>
2022-08-28 23:16:39 +0200

    more

--- lib/ruby/przypadki.rb
+++ lib/ruby/przypadki.rb
@@ -13,12 +13,23 @@ class Ruby::Przypadki::Error < StandardError
   # Your code goes here...
 end
 
+class String
+  unless method_defined?(:ends_with?)
+    def ends_with?(suffix)
+      return true if suffix.empty?
+
+      self[-suffix.length..] == suffix
+    end
+  end
+end
+
 module Ruby::Przypadki
   def self.dopelniacz(word)
-    if word[-1] == 'a'
-      "#{word[0...-1]}y"
-    else
-      "#{word}a"
-    end
+    return "#{word[0...-1]}i" if word.ends_with?('ka')
+    return "#{word[0...-1]}a" if word.ends_with?('ko')
+
+    return "#{word[0...-1]}y" if word[-1] == 'a'
+
+    "#{word}a"
   end
 end
--- spec/ruby/przypadki_spec.rb
+++ spec/ruby/przypadki_spec.rb
@@ -5,11 +5,21 @@ RSpec.describe Ruby::Przypadki do
     expect(Ruby::Przypadki::VERSION).not_to be_nil
   end
 
+  it 'checks suffixes' do
+    expect('test'.ends_with?('st')).to be(true)
+    expect('test'.ends_with?('xx')).to be(false)
+    expect('test'.ends_with?('test')).to be(true)
+    expect('test'.ends_with?('longtest')).to be(false)
+    expect('test'.ends_with?('')).to be(true)
+  end
+
   {
     'Tomasz' => 'Tomasza',
     'Sandra' => 'Sandry',
     'Grzegorz' => 'Grzegorza',
     'Aneta' => 'Anety',
+    'Mieszko' => 'Mieszka',
+    'Agnieszka' => 'Agnieszki',
   }.each do |input, output|
     it "calculates dopelniacz of #{input} (#{output})" do
       expect(Ruby::Przypadki.dopelniacz(input)).to eq(output)
